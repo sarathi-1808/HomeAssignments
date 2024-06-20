d082f4fae87b81b2f68abe6a34ce920b899a66c4
sarathi-1808 <sarathiravi45@gmail.com>
2024-06-20 06:07:13 +0530

    Add files via upload

---
+++ day3/FindMissingElement.java
@@ -0,0 +1,24 @@
+package weekn1.day3;
+
+import java.util.Arrays;
+
+public class FindMissingElement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[] = {1, 4, 3, 2, 8, 6, 7};
+		
+		Arrays.sort(a);
+		 for (int i = 0; i < a.length; i++) {
+			 if(a[i] != i+1) {
+				 System.out.println(i+1);
+				 break;
+			 }
+			
+		}
+		
+		
+
+	}
+
+}
